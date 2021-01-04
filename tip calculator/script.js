5cedd954281f0dbc07008cce02786355c495e36d
Ash Gandawa <17383007+TheMetacosmicEntity@users.noreply.github.com>
2021-01-04 12:51:51 +0200

    #100DaysOfCode Day 2: I refactored my code and wrapped the little script in an arrow function. I'm working on getting more comfortable with ES6 syntax. Also trying to get used to using Git as a normal part of my workflow. Still stumbling but I'm getting the hang of it.

--- tip calculator/script.js
+++ tip calculator/script.js
@@ -1,15 +1,22 @@
 let RestaurantRating = parseFloat(prompt("Please rate your experience at this restaurant out of 5:"));
 let WaiterRating = parseFloat(prompt("Kindly rate the waiter's service out of 5:"));
 let MealAmount = parseFloat(prompt("How much was your meal?"));
-let BaseTip;
 
-if (RestaurantRating <= 3) {
-    BaseTip = (WaiterRating/20) * MealAmount;
+let tipCalculator = (waiter, restaurant, meal) => {
+    let base;
+    if(restaurant <= 3) {
+        base = (waiter/20) * meal;
+    }
+    else {
+        base = 1.5 * ( (waiter/20) * meal );
+    }
+
+    let mealTotal = base + meal;
+    let totals = [base, mealTotal]
+    return totals;
 }
-else {
-    BaseTip = 1.5 * ((WaiterRating/20) * MealAmount);
-}
 
-let TotalBill = BaseTip + MealAmount;
+let MealTotal = tipCalculator(WaiterRating, RestaurantRating, MealAmount);
 
-alert(`Your base tip should be ${BaseTip}. Please feel free to add more. With the base tip, your bill total should be ${TotalBill}`);
+//console.log(MealTotal[0] + " " + MealTotal[1]);
+alert(`Your base tip should be ${MealTotal[0]}. Please feel free to add more. With the base tip, your bill total should be ${MealTotal[1]}`);
